1cf99c8b311225532d74e65c775844f2570c167e
neurxdreamz <kononovs@774gmail.com>
2026-03-03 18:24:23 +0300

    добавлены поля класса Book

--- LibraryAPP/ClassLibrary/Class1.cs
+++ LibraryAPP/ClassLibrary/Class1.cs
@@ -1,7 +1,19 @@
-﻿namespace ClassLibrary
+﻿using System;
+using System.Reflection;
+
+
+namespace ClassLibrary
 {
-    public class Class1
+    public class Book
     {
-        
+      
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public int id { get; private set; }
+        public string Genre { get; private set; }
+        public int Pages { get; private set; }
+        public double Price { get; private set; }
+
+
     }
 }
