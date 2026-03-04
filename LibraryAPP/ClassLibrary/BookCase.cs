47b847c9b85f56c4eb8233aa211d261812f88f6f
neurxdreamz <kononovs@774gmail.com>
2026-03-05 01:33:01 +0300

    добавил проверку на пустое имя в конструкторе

--- LibraryAPP/ClassLibrary/BookCase.cs
+++ LibraryAPP/ClassLibrary/BookCase.cs
@@ -4,14 +4,26 @@ using System.Text;
 
 namespace ClassLibrary
 {
+
+    /// <summary>
+    /// Класс "BookCase" представляет собой книжный шкаф, который может содержать определенное количество книг.
+    /// </summary>
     internal class BookCase
     {
         private List <Book> books;
         public int capacity {  get; private set; } 
         public string name { get; set; }
 
+        /// <summary>
+        /// Конструктор класса BookCase, инициализирует книжный шкаф с именем и вместимостью.
+        /// </summary>
         public BookCase (string Name, int Capacity)
         {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentException("Имя книжного шкафа не может быть пустым или состоять только из пробелов.");
+            }
+
             Name = name;
             Capacity = capacity;
 
@@ -31,6 +43,8 @@ namespace ClassLibrary
             }
             books.Add (book);
         }
+
+
 
     }
 }
