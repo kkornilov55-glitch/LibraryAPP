2faa8567f355bdbaa7cf9b40085a517fface7477
neurxdreamz <kononovs@774gmail.com>
2026-03-05 01:28:31 +0300

    сделал метод добавлени книги

--- LibraryAPP/ClassLibrary/BookCase.cs
+++ LibraryAPP/ClassLibrary/BookCase.cs
@@ -7,9 +7,30 @@ namespace ClassLibrary
     internal class BookCase
     {
         private List <Book> books;
+        public int capacity {  get; private set; } 
+        public string name { get; set; }
 
-        int capacity {  get; set; }
-        string name { get; set; }
+        public BookCase (string Name, int Capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+
+            books = new List <Book> ();
+        }
+
+        public void AddBook(Book book)
+        {
+            if (books.Count >= capacity)
+            {
+                throw new InvalidOperationException("Книжный шкаф полон. Невозможно добавить новую книгу.");
+            }
+            
+            if (book == null)
+            {
+                throw new ArgumentNullException("Невозможно добавить пустую книгу в книжный шкаф.");
+            }
+            books.Add (book);
+        }
 
     }
 }
