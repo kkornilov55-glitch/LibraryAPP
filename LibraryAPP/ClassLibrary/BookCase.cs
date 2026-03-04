79d3ce3def55e120c7e99a369c7e386049fc83fe
neurxdreamz <kononovs@774gmail.com>
2026-03-05 00:23:26 +0300

    создал отдельный .cs файл для второго класса

---
+++ LibraryAPP/ClassLibrary/BookCase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    internal class BookCase
+    {
+        private List <Book> books;
+
+        int capacity {  get; set; }
+        string name { get; set; }
+
+    }
+}
