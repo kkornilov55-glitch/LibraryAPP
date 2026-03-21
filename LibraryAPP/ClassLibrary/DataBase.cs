ec7edc04a4d06116ef67d52847f2961db3b326b6
neurxdreamz <kononovs774@mail.ru>
2026-03-21 19:02:30 +0300

    Начал DataBase

---
+++ LibraryAPP/ClassLibrary/DataBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class DataBase
+    {
+
+
+        public void ReadFile()
+        {
+            string FilePath = ("DataBase.txt");
+
+            List<(string Title, string Author)> LibraryBooks = new List<(string Title, string Author)> ();
+        }
+    }
+}
