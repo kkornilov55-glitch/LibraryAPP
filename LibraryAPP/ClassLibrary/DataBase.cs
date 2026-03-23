720d86f5d899a9c3aba52a622fd442eda7ff1355
neurxdreamz <kononovs@774gmail.com>
2026-03-23 23:59:56 +0300

    дописал метод ReadFile для бд

--- LibraryAPP/ClassLibrary/DataBase.cs
+++ LibraryAPP/ClassLibrary/DataBase.cs
@@ -7,12 +7,25 @@ namespace ClassLibrary
     public class DataBase
     {
 
+        string DataPath = "DataBase.txt";
 
         public void ReadFile()
         {
-            string FilePath = ("DataBase.txt");
+            string PrePath = "BookAuthor.txt";
 
             List<(string Title, string Author)> LibraryBooks = new List<(string Title, string Author)> ();
+
+            string[] lines = File.ReadAllLines(PrePath);
+
+            foreach (string line in lines)
+            {
+                var word = line.Split('|');
+
+                LibraryBooks.Add((word[0].Trim(), word[1].Trim()));                       
+            }
+
         }
+
     }
 }
+ 
